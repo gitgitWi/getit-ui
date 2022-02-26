6f9a2e6d838f3683b16bb1e01c6a6a2d62bad4fe
gitgitWi <wiii@kakao.com>
2022-02-26 23:44:12 +0900

    Feat (button) use library mode of Vite in `button`
    - set main, module, exports in `package.json`
    - add `emitDeclarationOnly` option to tsc

--- atoms/button/vite.config.ts
+++ atoms/button/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  /**
+   * for library mode
+   * @see {https://vitejs-kr.github.io/guide/build.html#library-mode}
+   * @see {https://vitejs.dev/guide/build.html#library-mode}
+   */
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.tsx'),
+      name: 'buttons',
+      fileName: (format) => `button.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+  },
+});
