8ace5958639cfa38e5682e1c0f27a2c975e915c4
gitgitWi <wiii@kakao.com>
2022-03-03 17:46:51 +0900

    Feat (button) add props & its interface, story

---
+++ stories/src/Button.stories.tsx
@@ -0,0 +1,31 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { Button } from '@getit-ui/button';
+
+export default {
+  title: 'atoms/button',
+  component: Button,
+  argTypes: {
+    classNames: {
+      control: 'object',
+      type: { name: 'other', value: '' },
+      defaultValue: [],
+    },
+    textContent: {
+      control: 'string',
+      type: { name: 'string', required: true },
+      defaultValue: 'button',
+    },
+  },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const DefaultTemplate = Template.bind({});
+DefaultTemplate.args = {};
+
+export const RedButton = Template.bind({});
+RedButton.args = {
+  classNames: [],
+  textContent: 'red button',
+};
